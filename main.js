34194f4753f9051a93fe34b56733168f5c3c257c
alexander <130918131+alekeysmith@users.noreply.github.com>
2026-05-21 21:19:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -35,11 +35,10 @@ class User {
 
 // In-memory data
 let users = [
-    new User(1, 'John Doe', 'johndoe', 'john@example.com', 'password123', new Date(), 'john.jpg', 'admin'),
-    new User(2, 'Jane Smith', 'janesmith', 'jane@example.com', 'password123', new Date(), 'jane.jpg', 'user'),
+    new User(1, 'Joan Alexander Julian', 'joanalexander', 'joanalexander@gmail.com', 'password123', new Date(), 'joan.jpg', 'admin'),
+    new User(2, 'Ingenieria de Sistemas', 'sistemas', 'sistemas@gmail.com', 'password123', new Date(), 'sistemas.jpg', 'user'),
     new User(3, 'Robert Brown', 'robbrown', 'robert@example.com', 'password123', new Date(), 'robert.jpg', 'user')
 ];
-
 // CRUD Endpoints
 
 // Get all users
